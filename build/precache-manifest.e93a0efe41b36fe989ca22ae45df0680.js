1366f725f6dd748e0ccf969f908c45578663bc18
chebyrash <pd.stroganov@gmail.com>
2019-09-16 15:52:15 +0100

    new build

---
+++ build/precache-manifest.e93a0efe41b36fe989ca22ae45df0680.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ce7c18bfa043651b398afeea28285e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c1a5d62c41da9ea6539",
+    "url": "/static/css/main.cde11cda.chunk.css"
+  },
+  {
+    "revision": "85c0eb56472870a8577e",
+    "url": "/static/js/2.7658b655.chunk.js"
+  },
+  {
+    "revision": "1c1a5d62c41da9ea6539",
+    "url": "/static/js/main.f602575b.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
