4a60b1ec1cd17866d2778938f572fa9757d12809
chebyrash <pd.stroganov@gmail.com>
2019-11-15 18:16:21 +0000

    build upload

---
+++ build/precache-manifest.2b8aad5988cf631363b5c748d8317e86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f293803a996f9309d75bdf31402f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "244b8d7d40b810290a5b",
+    "url": "/static/css/main.5eb30739.chunk.css"
+  },
+  {
+    "revision": "d24b47a23b19f86b7730",
+    "url": "/static/js/2.89e2ecd2.chunk.js"
+  },
+  {
+    "revision": "244b8d7d40b810290a5b",
+    "url": "/static/js/main.2b70fc88.chunk.js"
+  },
+  {
+    "revision": "aaebcec93c1856593ad7",
+    "url": "/static/js/runtime-main.4a365033.js"
+  }
+]);
